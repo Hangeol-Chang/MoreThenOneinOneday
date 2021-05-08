a754b058f2e0a9cae4337b443e4b863c378037e3
Hangeol-Chang <57909592+Hangeol-Chang@users.noreply.github.com>
2021-05-08 19:12:20 +0900

    초기 커밋.

---
+++ Project1/studying.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[]) {
+	std::cout << "Hello, github" << std::endl;
+	
+	return EXIT_SUCCESS;
+}
