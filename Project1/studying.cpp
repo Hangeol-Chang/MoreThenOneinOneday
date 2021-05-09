077b584be1adddcf02ed11b62bc5d55734754f08
Hangeol-Chang <57909592+Hangeol-Chang@users.noreply.github.com>
2021-05-09 18:34:09 +0900

    studing headerfilers

--- Project1/studying.cpp
+++ Project1/studying.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
-#include <cstdlib>
+#include <string>
 
-using std::cout;
-using std::endl;
+using std::cout;		using std::cin;
+using std::endl;		using std::string;
 
-int main(int argc, char* argv[]) {
-	std::cout << "Hello, github" << std::endl;
+int main() {
+	cout << "ent name : ";
 	
-	return EXIT_SUCCESS;
+	string name;
+	cin >> name;
+	
+	const string greeting = "Hello, " + name + "!";
+	
+	const int pad = 1;
+	
+	const int rows = pad * 2 + 3;
+	const string::size_type cols = greeting.size() + pad * 2 + 2;
+
+	cout << endl;
+
+	for (int r = 0; r != rows; ++r) {
+		string::size_type c = 0;
+		
+		while (c != cols) {
+			
+			if (r == pad + 1 && c == pad + 1) {
+				cout << greeting;
+				c += greeting.size();
+			}
+			else {
+				
+				if (r == 0 || r == rows - 1 ||
+					c == 0 || c == cols - 1)
+					cout << "*";
+				else
+					cout << " ";
+				++c;
+			}
+		}
+	}
 }
