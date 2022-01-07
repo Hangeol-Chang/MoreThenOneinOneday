1fa7e8c08e77ecae1763e49ba306504092dc523e
Hangeol-Chang <hgchang1@naver.com>
2022-01-08 02:02:05 +0900

    Create BJ1010.java

---
+++ BJ1010.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BJ1010 {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        
+        int n = in.nextInt();
+
+
+    }
+}
