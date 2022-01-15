e2bc29448c28ba5b365b76ed177c2a59a4a12b61
hihangeol <hgchang1@naver.com>
2022-01-16 02:16:05 +0900

    Create BJ1152.java

---
+++ BJ1152.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BJ1152 {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+
+        String[] wor = in.nextLine().split(" ");
+        int len = wor.length;
+        if(len > 0) if(wor[0] == "") len--;
+        System.out.println(len);
+        in.close();
+    }
+}
