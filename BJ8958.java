cf5c5e08825797441cffa824956d7791d681cc8b
hihangeol <hgchang1@naver.com>
2022-01-15 23:55:45 +0900

    Update BJ8958.java

--- BJ8958.java
+++ BJ8958.java
@@ -2,6 +2,23 @@ import java.util.Scanner;
 
 public class BJ8958 {
     public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
         
+        int t = in.nextInt();
+        in.nextLine();
+        
+        for(int i = 0; i < t; i++){
+            String ox = in.nextLine();
+            int count = 0;
+            int score = 0;
+
+            for(int j = 0; j < ox.length(); j++){
+                if( ox.charAt(j) == 'O' ){
+                    count++;
+                    score += count;
+                }else count = 0;
+            }
+            System.out.println(score);
+        }
     }
 }
