2dc8f4652e32418d8dd70aa00b5fac840ff62e4f
hihangeol <57909592+Hangeol-Chang@users.noreply.github.com>
2022-01-18 17:47:06 +0900

    Update CU1259.java

--- CU1259.java
+++ CU1259.java
@@ -1,4 +1,16 @@
 import java.util.Scanner;
 public class CU1259 {
-    
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        
+        int n = in.nextInt();
+        if(!(n % 2 == 0)) n--;
+        int sum = 0;
+        
+        for(int i = n; i > 0; i -= 2){
+            sum += i;
+        }
+        
+        System.out.println(sum);
+    }
 }
