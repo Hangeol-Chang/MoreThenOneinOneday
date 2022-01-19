562fa5f0ad936d7624d229b6db8454beff4e6d71
hihangeol <hgchang1@naver.com>
2022-01-19 20:25:57 +0900

    220119

--- CU1271.java
+++ CU1271.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 public class CU1271 {
   public static void main(String args[]){
-    
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int max = 0;
+    for(int i = 0; i < n; i++){
+      int tmp = in.nextInt();
+      if(tmp > max) max = tmp;
+    }
+    in.close();
+    System.out.println(max);
   }
 }
---
+++ CU1474.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class CU1474{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        
+    }
+}
