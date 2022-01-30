98142ea68fa6504cc6b2e9194be24e6953ecfdf9
hihangeol <hgchang1@naver.com>
2022-01-30 17:07:26 +0900

    Create BJ2798.java

---
+++ BJ2798.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.stream.Stream;
+public class BJ2798 {
+    public static int maxnum = 0;
+    public static int goal = 0;
+    public static int[] nums;
+    public static int n = 0;
+
+    public static void calcmax(int nowindex, int nownum, int count){
+        for(int i = nowindex; i < n; i++){
+            int tmp = nownum + nums[i];
+            int tmpcount = count + 1;
+            //System.out.println("현재 인덱스 : " + i + ", 현재 수 : " + tmp + ", 현재 더한 수 : " + tmpcount + ", 현재 맥스값 : " + maxnum);
+            if(tmp <= goal){
+                if(tmpcount == 3){
+                    if(tmp > maxnum) maxnum = tmp;
+                }else if (i + 1 < n){
+                    calcmax(i + 1, tmp, tmpcount);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        n = in.nextInt();
+        goal = in.nextInt();            //static으로 선언한 goal에 불러옴.
+
+        in.nextLine();                  //버퍼 비우기
+
+        nums = Stream.of(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        in.close();
+        
+        calcmax(0,0,0);
+        
+        System.out.println(maxnum);
+    }
+    
+}
