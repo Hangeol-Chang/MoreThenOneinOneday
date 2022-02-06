d4df1586b5e1a7d6a7efa8f8241e13c4c1de8023
Hangeol-Chang <57909592+Hangeol-Chang@users.noreply.github.com>
2022-02-06 18:38:54 +0900

    0206

---
+++ BJ11866.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class BJ11866 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        Queue que = new \
+
+        //더해가면서 n보다 크면 n으로 나눈 나머지 잡기.
+
+        int n = in.nextInt();
+        int k = in.nextInt();
+        in.nextLine();     
+
+        for(int i = 0; i < n; i++){
+            
+
+        }
+        System.out.println(sb);
+        in.close();
+    }
+}
