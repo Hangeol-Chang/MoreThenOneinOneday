b6f940521dc0e9318474bc52588fcc137e5f03a3
hihangeol <hgchang1@naver.com>
2022-02-07 21:59:26 +0900

    0207

--- BJ11866.java
+++ BJ11866.java
@@ -6,18 +6,41 @@ public class BJ11866 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
-        Queue que = new \
+        LinkedList<Integer> li = new LinkedList<>();
 
         //더해가면서 n보다 크면 n으로 나눈 나머지 잡기.
 
         int n = in.nextInt();
         int k = in.nextInt();
-        in.nextLine();     
+        in.nextLine();      //버퍼 비우기
 
-        for(int i = 0; i < n; i++){
-            
+        int index = k;
+        int size = n;
+        
+        //링크리스트 채워놓기.
+        for(int i = 0; i <= n; i++) li.add(i);
+        
+        //뽑아내며 문자열 만들기
+        //2번째부터는 , 를 붙여야하니, 첫번째꺼는 따로 붙임.
+        sb.append("<");
+        sb.append(li.remove(index--));
+        size--;
+        
+        while(size > 0) {
+            //콤마를 찍고 시작
+            sb.append(", ");
+            index += k;
+            if(index > size) {
+                index %= size;
+                if(index == 0) index = size;
+            }
 
+            //System.out.println("index : " + index + ", size :" + size + ", comp : " + li.get(index));
+            sb.append(li.remove(index--));
+            size--;
         }
+        sb.append(">");
+
         System.out.println(sb);
         in.close();
     }
--- BJ3226.java
+++ BJ3226.java
@@ -1,9 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
 
 public class BJ3226 {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws NumberFormatException, IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(in.readLine());
+        int pay = 0;
 
+        for(int i = 0; i < n; i++){
+            int[] hmd = Stream.of(in.readLine().split(" |:")).mapToInt(Integer::parseInt).toArray();
+            
+            pay += hmd[2]*5;
+            if (hmd[1] + hmd[2] > 60){
+                
+            }
+            
+        }
         
     }
 }
+/*
+0000 ~ 0700
+
+0700 ~ 1900
+
+1900 ~ 0000
+
+19로 나눴을 때, 나머지가 7 ~ 18 이면 
+*/
