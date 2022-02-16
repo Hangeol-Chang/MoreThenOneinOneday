85e047a8733b974a3ffe174e15f79aa803eb8875
hihangeol <hgchang1@naver.com>
2022-02-16 17:57:31 +0900

    0216

---
+++ eclipse/src/SWEA1218_괄호짝짓기.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class SWEA1218_괄호짝짓기 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in); 
+		StringBuilder sb = new StringBuilder();
+		
+		for(int t = 1; t <= 10; t++) {
+			Stack<Integer> stack = new Stack<>();
+			sb.append("#" + t + " ");
+			//입력받기
+			int len = in.nextInt();
+			in.nextLine();
+			
+			String str = in.nextLine();
+			
+			String search = "({[<)}]>";
+			
+			boolean pass = true;
+			for(int i = 0; i < len; i++) {
+				int tmp = search.indexOf(str.charAt(i));
+				//System.out.print(tmp);
+				if(tmp > 3) {
+					if(stack.peek() == tmp%4) stack.pop();
+					else { pass = false; break; }
+				} else { stack.push(tmp); }
+			}
+			//System.out.println(pass + " " + stack.isEmpty());
+			if(pass && stack.isEmpty()) sb.append("1\n");
+			else sb.append("0\n");
+		}
+		System.out.println(sb);
+		in.close();
+	}
+}
