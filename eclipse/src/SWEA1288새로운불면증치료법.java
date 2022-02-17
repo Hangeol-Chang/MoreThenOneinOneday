7a28ff334532cb6d2863a31518b90fba65d5decb
Hangeol-Chang <57909592+Hangeol-Chang@users.noreply.github.com>
2022-02-17 22:43:03 +0900

    0217

---
+++ eclipse/src/SWEA1288새로운불면증치료법.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class SWEA1288새로운불면증치료법 {
+
+  public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      StringBuilder sb = new StringBuilder();
+      int T = in.nextInt();
+      
+      for(int t = 1; t <= T; t++){
+          boolean[] isin = new boolean[10];
+          int n = in.nextInt();
+          int n2 = 0;
+          
+          int checker = 10;
+          int k = 1;
+          while(checker > 0) {
+              n2  = k*n;
+              int tmp = n2;
+              while(tmp > 0){
+                  if(!isin[tmp%10]) {
+                      isin[tmp%10] = true;
+                      checker--;
+                  }
+                  tmp /= 10;
+              }
+              k++;
+          }
+          sb.append("#" + t + " " + n2 + "\n");
+      }
+      System.out.println(sb);
+
+  }
+}
