da0564cc4c1e7584962f4fb4ce3044412b4af488
Hangeol-Chang <57909592+Hangeol-Chang@users.noreply.github.com>
2022-02-18 17:46:01 +0900

    0218

---
+++ eclipse/src/BJ2635_수이어가기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * 100 62 38 24 14 10 4 6
+ * 100 50 50 0
+ * 100 60 40 20 20 0 20
+ * 100 70 30 40 
+ * 100 66 34 17 17 0
+ * 100 75 25 50 < 이렇게 튀어오르면 안됨.
+ * 100 68 34 34 0
+ * 100 69 31 38
+ * 1/2 n 과 3/4 n 사이
+ */
+
+public class BJ2635_수이어가기 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+	}
+}
---
+++ eclipse/src/SWEA7272_안경이없어.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * ADOPQR B
+ */
+public class SWEA7272_안경이없어 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int T = in.nextInt();
+		for(int t = 1; t <= T; t++) {
+			
+			
+			sb.append("#" + t + " ");
+		}
+		
+	}
+}
