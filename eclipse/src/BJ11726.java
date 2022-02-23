20ae7556dea78c93e2088fd3d5ecdee880d4201c
hihangeol <hgchang1@naver.com>
2022-02-23 17:34:53 +0900

    0223

---
+++ eclipse/src/BJ11726.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/*
+ * 2 2
+ * 3 3
+ * 4 5
+ * 5 
+ * 
+ * 2와 1로 만들 수 있는 양 찾기.
+ */
+
+public class BJ11726 {
+	static int count = 0;
+	public static void main(String[] arsg) {
+		Scanner in = new Scanner(System.in);
+	}
+}
