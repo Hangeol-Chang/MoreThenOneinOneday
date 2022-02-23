2a8c73a39e8109509051b3d39ee40bc6950c4c8c
hihangeol <hgchang1@naver.com>
2022-02-23 18:39:27 +0900

    0223

--- eclipse/src/BJ11727_2xn타일링2.java
+++ eclipse/src/BJ11727_2xn타일링2.java
@@ -1,24 +1,13 @@
 import java.util.Scanner;
-/*
- * 2 2
- * 3 3
- * 4 5
- * 5 8
- * 6 13
- * 7 21
- * 8 34
- * 9 55
- * 
- * 2와 1로 만들 수 있는 양 찾기.
- */
 
-public class BJ11726 {
+
+public class BJ11727_2xn타일링2 {
 	public static void main(String[] arsg) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int[] nums = new int[n+2];
 		nums[1] = 1;
-		nums[2] = 2;
+		nums[2] = 3;
 		System.out.println(counting(n, nums)); 
 		in.close();
 	}
@@ -26,7 +15,7 @@ public class BJ11726 {
 	public static int counting(int n, int[] nums) {
 		if(nums[n] != 0) return nums[n];
 		else {
-			nums[n] = (counting(n-1, nums) + counting(n-2, nums))%10007;
+			nums[n] = (counting(n-1, nums) + 2*counting(n-2, nums))%10007;
 			return nums[n];
 		}
 	}
