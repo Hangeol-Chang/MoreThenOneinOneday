c86f3e2cee9496babd60b2db6f6d2d9586e6b902
hihangeol <hgchang1@naver.com>
2022-03-01 17:31:33 +0900

    0301

--- eclipse/src/ChangHangeol/BJ11724_연결요소의개수.java
+++ eclipse/src/ChangHangeol/BJ11724_연결요소의개수.java
@@ -1,4 +1,5 @@
 package ChangHangeol;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -25,6 +26,7 @@ public class BJ11724_연결요소의개수 {
             st = new StringTokenizer(br.readLine());
             int t1 = Integer.parseInt(st.nextToken());
             int t2 = Integer.parseInt(st.nextToken());
+            
             grp[t1][t2] = 1;
             grp[t2][t1] = 1;
         }
@@ -42,49 +44,19 @@ public class BJ11724_연결요소의개수 {
         
         
         int count = 0;
-        for(int i = 1; i <= n; i++){
-            for(int j = i+1; j <= n; j++){
-                if(grp[i][j] == 1 && !visit[i]) {
-            		visit[i] = true;
-            		visitcount--;
-            		erase(i);
-            		count++;
-//                	System.out.println("지우기 시작");
-                    
-//                    System.out.println("하나 끝남");
-//                    for(int p = 1; p <= n; p++) {
-//                    	for(int e = 1; e <= n; e++) {
-//                    		System.out.print(grp[p][e] + " ");
-//                    	}
-//                    	System.out.println();
-//                    }
-//                    System.out.println("여기까지\n");
-                    break;
-                }
-            }
+        for(int i = 1; i <= n; i++) {
+        	if(!visit[i]) {
+        		dfs(i);
+        		count++;
+        	}
         }
-        System.out.println(count + visitcount);
+        System.out.println(count);
+        
     }
-
-    public static void erase(int i){
-	        for(int jj = j; jj <= n; jj++){
-	            if(grp[i][jj] == 1) {
-	                grp[i][jj] = 0;
-	                grp[jj][i] = 0;
-	                
-	//                System.out.println("지운 위치 : " + i + " " + jj);
-	//                for(int p = 1; p <= n; p++) {
-	//                	for(int e = 1; e <= n; e++) {
-	//                		System.out.print(grp[p][e] + " ");
-	//                	}
-	//                	System.out.println();
-	//                }
-	//                System.out.println(" ");
-	                
-	                erase(jj, jj);
-	                
-	            }
-	        }
-        }
+    public static void dfs(int i) {
+    	visit[i] = true;
+    	for(int j = i+1; j <= n; j++) {
+    		if(grp[i][j] == 1) dfs(j);
+    	}
     }
 }
