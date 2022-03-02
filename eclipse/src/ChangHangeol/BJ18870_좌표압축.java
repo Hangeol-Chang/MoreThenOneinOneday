9c05d0a24d8c8f92ccbf89f1e8526dd984d2c663
hihangeol <hgchang1@naver.com>
2022-03-02 09:19:48 +0900

    0302

--- eclipse/src/ChangHangeol/BJ18870_좌표압축.java
+++ eclipse/src/ChangHangeol/BJ18870_좌표압축.java
@@ -1,7 +1,22 @@
 package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.stream.Stream;
+
 public class BJ18870_좌표압축 {
-    public static void main(String[] args){
-        //제일 작은걸 0으로 잡고,
+	static int[] nums;
+	
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	int n = Integer.parseInt(br.readLine());
+    	nums = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    	//제일 작은걸 0으로 잡고,
         //쓸 수를 1씩 올려가면서, 찾게 되는 수에 대입.
 
         //우선순위 큐에 입력된 값들을 다 넣고,(중복은 제거)
