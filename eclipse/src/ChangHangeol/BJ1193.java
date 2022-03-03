4c9dfc700c5b074cecf7b543b66024ff97f11eda
hihangeol <hgchang1@naver.com>
2022-03-03 19:19:45 +0900

    0303

---
+++ eclipse/src/ChangHangeol/BJ1193.java
@@ -0,0 +1,21 @@
+package ChangHangeol;
+
+import java.util.Scanner;
+
+public class BJ1193 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        int t = 0;
+        int i = 0;
+        while(t < n){
+        	t += ++i;
+        }
+        t -= i;
+        if(i%2 == 1) System.out.println(i+1-(n-t) + "/"+ (n-t));
+        else System.out.println((n-t) + "/" + (i+1-(n-t)));
+        in.close();
+    }
+}
