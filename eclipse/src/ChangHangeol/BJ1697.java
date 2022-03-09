94af50c949b6efbfa14d6f7ecc49820bc6f32660
hihangeol <hgchang1@naver.com>
2022-03-09 18:53:52 +0900

    0309

--- eclipse/src/ChangHangeol/BJ1697.java
+++ eclipse/src/ChangHangeol/BJ1697.java
@@ -20,21 +20,34 @@ public class BJ1697 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int num = Integer.parseInt(st.nextToken());
 		goal = Integer.parseInt(st.nextToken());
-		maxcount = goal - num;
+		maxcount = Math.abs(goal - num);
 		
-		int result = calc(num, 0);
+		int result = calc(num, 0, 0);
 		System.out.println(result);
 	}
 	
-	public static int calc(int nownum, int count) {
-		System.out.println("지금 수 : " + nownum + ", count : " + count);
+	public static int calc(int nownum, int count, int idx) {
+//		System.out.println("지금 수 : " + nownum + ", count : " + count);
 		if(nownum == goal || count >= maxcount) return count;
+		if(nownum < 0) return maxcount;
+		if(nownum - goal > maxcount - count) return maxcount;
 		
-		else if(nownum < goal) {
-			return Math.min(calc(nownum+1, count+1), calc(nownum*2, count+1));
-		}else {
-			return calc(nownum-1, count+1);
+		int t1 = 0;
+		switch(idx) {
+		case 1:
+			t1 = calc(nownum+1, count+1, 1);
+			break;
+		case -1:
+			t1 = calc(nownum-1, count+1, -1);
+			break;
+		case 0:
+			t1 = Math.min(calc(nownum+1, count+1, 1), calc(nownum-1, count+1, -1));
+			break;
 		}
+		int t2= calc(nownum*2, count+1, 0);
+		int t = Math.min(t1, t2);
+		maxcount = Math.min(t, maxcount);
+		return t;
 	}
 }
 
