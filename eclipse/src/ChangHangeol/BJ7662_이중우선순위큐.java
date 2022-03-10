b72fb4d60f36927f3bb69da6f15780b07b54873e
hihangeol <hgchang1@naver.com>
2022-03-10 20:26:01 +0900

    0310

--- eclipse/src/ChangHangeol/BJ7662_이중우선순위큐.java
+++ eclipse/src/ChangHangeol/BJ7662_이중우선순위큐.java
@@ -11,7 +11,7 @@ public class BJ7662_이중우선순위큐 {
 			LinkedList<Integer> que = new LinkedList<>();
 			int max = 0;
 			public void add(int num) {
-				
+				//환영합니다 여러분.
 			}
 		}
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
