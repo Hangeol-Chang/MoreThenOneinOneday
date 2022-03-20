455bcccf54ada6cd1145c2be74a6e7ce58178b2c
hihangeol <hgchang1@naver.com>
2022-03-20 20:10:32 +0900

    0320

---
+++ eclipse/src/ChangHangeol/BJ2908_상수.java
@@ -0,0 +1,24 @@
+package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2908_상수 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] a = new int[2];
+		StringBuilder sb;
+		for(int i = 0; i < 2; i++) {
+			sb = new StringBuilder();
+			sb.append(st.nextToken()).reverse();
+			
+			a[i] = Integer.parseInt(sb.toString());
+		}
+		
+		System.out.println(a[0]>a[1] ? a[0] : a[1]);
+	}
+}
