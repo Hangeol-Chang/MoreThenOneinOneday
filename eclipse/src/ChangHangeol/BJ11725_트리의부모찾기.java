afa98c24f198137d86c626af2f3b45fca5470359
Your Name <hgchang1@naver.com>
2022-03-24 18:04:34 +0900

    0324

--- eclipse/src/ChangHangeol/BJ11725_트리의부모찾기.java
+++ eclipse/src/ChangHangeol/BJ11725_트리의부모찾기.java
@@ -3,46 +3,44 @@ package ChangHangeol;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.stream.Stream;
 
 public class BJ11725_트리의부모찾기 {
-	static boolean[][] graph;
-	static short[] parents;
+	static int[] parents;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		graph = new boolean[N+1][N+1];
-		parents = new short[N+1];
-		Queue<Short> que = new LinkedList<>();
+		ArrayList<Integer>[] graph = new ArrayList[N+1];
+		for(int i = 1; i <= N; i++) graph[i] = new ArrayList<Integer>();
+		parents = new int[N+1];
+		Queue<Integer> que = new LinkedList<>();
 		
-		int[] nodes;
+		int[] node;
 		for(int i = 0; i < N-1; i++) {
-			nodes = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			
-			graph[nodes[0]][nodes[1]] = true;
-			graph[nodes[1]][nodes[0]] = true;
+			node = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			graph[node[0]].add(node[1]);
+			graph[node[1]].add(node[0]);
 		}
 		// 그래프 입력 끝.
 		
 		// 1부터 시작해서,
 		// 큐에 부모를 다 떄려 넣으면서 계산하기.
-		que.add((short) 1);
-		while(!que.isEmpty()) {
-			short p = que.poll();
-			
-			for(short i = 2; i <= N; i++) {
-				if(graph[p][i]) {
-					que.add(i);
-					parents[i] = p;
-					graph[p][i] = false;
-					graph[i][p] = false;
+		que.add(1);
+		while(!que.isEmpty()){
+			int parent = que.poll();
+			for(int nd : graph[parent]) {
+				// 연결된 노드에 방문한 적이 없으면
+				if(parents[nd] == 0) {
+
+					que.add(nd);
+					parents[nd] = parent;
 				}
 			}
 		}
-		
 		StringBuilder sb = new StringBuilder();
 		for(int i = 2; i <= N; i++) { sb.append(parents[i] + "\n"); }
 		System.out.println(sb);
---
+++ eclipse/src/ChangHangeol/BJ15663_N과M9.java
@@ -0,0 +1,5 @@
+package ChangHangeol;
+
+public class BJ15663_N과M9 {
+//해쉬맵
+}
