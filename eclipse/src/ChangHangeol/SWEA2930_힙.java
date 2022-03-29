ca9f51491bfc2bea79ce88895a73c44492704900
Your Name <hgchang1@naver.com>
2022-03-29 12:52:07 +0900

    0329

--- eclipse/src/ChangHangeol/SWEA2930_힙.java
+++ eclipse/src/ChangHangeol/SWEA2930_힙.java
@@ -3,6 +3,7 @@ package ChangHangeol;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
@@ -10,65 +11,89 @@ public class SWEA2930_힙 {
 	static StringBuilder sb = new StringBuilder();
 	
 	static class Tree{
-		static int size = 0;
-		HashMap<Integer, Integer> tree = new HashMap<>();
-		
-		// data, idx 순으로 저장할거임.
-		public void addtree(int data) {
-			size++;
-			tree.put(size, data);
+		ArrayList<Integer> tree = new ArrayList<>();
+		Tree(){
+			tree.add(Integer.MAX_VALUE);
 		}
-		public void delnode() {	
-			int data = tree.get(size);
-			sb.append(tree.get(1) + " ");
-			tree.put(1,  data);
-			tree.remove(size--);
-			// 1번 자리를 마지막걸로 덮어씌우고, 마지막꺼 날리고 사이즈 1 빼기.
+		// data 순으로 저장할거임.
+		public void addnode(int data) {
+			tree.add(data);
+			int idx = tree.size()-1;
 			
-			int idx = 1;
-			// key, value로 저장
-			int[] idf1 = {idx*2, tree.get(idx*2) != null ? tree.get(idx*2) : 0};
-			int[] idf2 = {idx*2+1, tree.get(idx*2+1)};
-			int[] maxidf = new int[2];
-			while( idf1[1] > data || idf2[2] > data) {
-				if(idf1[1] > idf2[1]) maxidf = idf1;
-				else 				  maxidf = idf2;
-				// 최댓값을 골라오고
-				
-				tree.put(idx, maxidf[1]);
-				// 최대값 바꾸고,
-				idx = maxidf[0];
-				// 내 다음 위치 설정.
-				tree.put(idx, data);
-				// 바뀐 위치에 내꺼 넣기.
+			while(data > tree.get(idx/2)) {
+				switchdata(data, tree.get(idx/2), idx, idx/2);
+				idx /= 2;
 			}
 		}
-		Tree(){
+		public void delnode() {
+			// 마지막꺼를 맨 앞에 넣고, 마지막꺼 지우기.
+			if(tree.size() == 1) { sb.append(" -1"); return; }
 			
+			int idx = 1;
+			int lastidx = tree.size() -1;
+			int data = tree.get(lastidx);
+			sb.append(" " + tree.get(1));
+			tree.set(idx, data);
+			tree.remove(lastidx);
+			
+			int[] idf = getdata(idx);
+			while(data < idf[0] || data < idf[1]) {
+				if(idf[0] < idf[1]) {
+					switchdata(data, idf[1], idx, idx*2+1);
+					idx = idx*2 +1;
+				}
+				else {
+					switchdata(data, idf[0], idx, idx*2);
+					idx *= 2;
+				}
+				idf = getdata(idx);
+			}
 		}
+		
+		public int[] getdata(int idx) {
+			int idf1 = 0;
+			int idf2 = 0;
+			if(idx*2 < tree.size()) idf1 = tree.get(idx*2);
+			if(idx*2 +1 < tree.size()) idf2 = tree.get(idx*2 +1);
+			
+			int[] idf = {idf1, idf2};
+			return idf;
+		}
+		
+		
+		public void switchdata(int a, int b, int idx1, int idx2) {
+			tree.set(idx1, b);
+			tree.set(idx2, a);
+		}
+
 	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		
-		Tree tree;
+		Tree tree = new Tree();
 		StringTokenizer st;
 		for(int t = 1; t <= T; t++) {
 			int n = Integer.parseInt(br.readLine());
+			sb.append("#" + t);
 			tree = new Tree();
 			for(int i = 0; i < n; i++) {
-				sb.append("#" + t + " ");
 				
 				st = new StringTokenizer(br.readLine());
 				int act = Integer.parseInt(st.nextToken());
 				
 				switch(act) {
-				
+				case 1 :
+					tree.addnode(Integer.parseInt(st.nextToken()));
+					break;
+				case 2 :
+					tree.delnode();
+					break;
 				}
 			}
+			sb.append("\n");
 		}
-		HashMap<Integer, Integer> map = new HashMap<>();
-		System.out.println(map.get(1));
+		System.out.println(sb);
 	}
 }
