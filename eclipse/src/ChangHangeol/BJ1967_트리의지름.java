ad83b8d5502d9641a2dee124e59545daf5d19bd5
Your Name <hgchang1@naver.com>
2022-04-05 16:07:23 +0900

    0405

--- eclipse/src/ChangHangeol/BJ1967_트리의지름.java
+++ eclipse/src/ChangHangeol/BJ1967_트리의지름.java
@@ -15,22 +15,57 @@ public class BJ1967_트리의지름 {
 			return this.w - o.w;
 		}
 	}
+	static node maxd = new node(0, 0);
+	static ArrayList<node>[] tree;
+	static boolean visited[];
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// 1에서 시작해서 왼쪽 끝, 오른쪽 끝 위치 재기
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
 		
-		ArrayList<node>[] tree = new ArrayList[N+1];
+		tree = new ArrayList[N+1];
 		for(int i = 0; i <= N; i++) tree[i] = new ArrayList<>();
+		visited = new boolean[N+1];
 		for(int i = 0; i < N-1; i++) {
-			st = new StringTokenizer(br.readLine());				
-			tree[Integer.parseInt(st.nextToken())].add(new node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()))); 
+			st = new StringTokenizer(br.readLine());		
+			// u에서 v로 갈 때는 w의 가중치가 필요함.
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			tree[u].add(new node(v, w)); 
+			tree[v].add(new node(u, w));
 		}
 		// 입력받기 끝.
 		
+		visited[1] = true;
+		findpath(1, 0);
 		
+		visited = new boolean[N+1];
+		visited[maxd.v] = true;
+		findpath(maxd.v, 0);
+		System.out.println(maxd.w);
 	}
-	// 모든 말단 노드들에서 
-	public void 
+	
+	// 가장 먼 정점 찾기.
+	public static void findpath(int loc, int len) {
+		boolean cango = false;
+		for(node nd : tree[loc]) {
+			if(visited[nd.v]) continue;
+			visited[nd.v] = true;
+			cango = true;
+			
+			findpath(nd.v, len + nd.w);
+			visited[nd.v] = false; 
+		}
+		
+		if(!cango) {
+			if(maxd.w < len) {
+				// 현재 위치를 저장.
+				maxd.v = loc;
+				maxd.w = len;
+			}
+			return;
+		}
+	}
 }
