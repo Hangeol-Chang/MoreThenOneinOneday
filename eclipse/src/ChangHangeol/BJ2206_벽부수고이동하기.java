22869f2ab6efd9736b20b768962b6320ff4f70f5
Your Name <hgchang1@naver.com>
2022-04-06 19:02:18 +0900

    0406

--- eclipse/src/ChangHangeol/BJ2206_벽부수고이동하기.java
+++ eclipse/src/ChangHangeol/BJ2206_벽부수고이동하기.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-// 틀렸다 모르겄다.
+// 시간초과
 
 // 벽을 부술 수 있으면 그냥 1을 밟아버리기.
 // 무조건 1,1에서 시작
@@ -33,70 +33,31 @@ public class BJ2206_벽부수고이동하기 {
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 		
-		// 테두리를 -1로 초기화.
-		map = new int[M+2][N+2];
-		for(int i = 0; i < M+2; i++) {
-			map[i][0]   = -1;
-			map[i][N+1] = -1;
-		}
-		for(int i = 0; i < N+2; i++) {
-			map[0][i]   = -1;
-			map[M+1][i] = -1;
-		}
+		// 테두리를 2로 초기화.
+		map = new int[M][N];
 		// map 입력받기
-		for(int r = 1; r <= M; r++) {
+		for(int r = 0; r < M; r++) {
 			String ref = br.readLine();
-			for(int c = 1; c <= N; c++) {
-				map[r][c] = ref.charAt(c-1) - '0';
+			for(int c = 0; c < N; c++) {
+				map[r][c] = ref.charAt(c) - '0';
 			}
 		}
 		// 입력받기 끝.
 		
 		dist = M * N;
-		dfs(1, 1, 1, 1);
-		
-		Queue<loc> que = new LinkedList<>();
 		// 출발 때 길이를 2로 잡고 시작.
-		que.add(new loc(1, 1, 2, true));
-		map[1][1] = 1;
+		map[0][0] = 2;
+		dfs(0, 0, 2, 1);
 		
-		// 벽을 부수지 않고 출구를 찾아보기.
-		while(!que.isEmpty()) {
-			loc nowloc = que.poll();
-			for(int d = 0; d < 4; d++) {
-				int nr = nowloc.r + dr[d];
-				int nc = nowloc.c + dc[d];
-				
-				if(map[nr][nc] == 0) {
-					
-					map[nr][nc] = nowloc.len + 1;
-					que.add(new loc(nr, nc, map[nr][nc], true));
-				}
-				// 벽을 부술 수 있는 상황에 대해, 벽을 부수고 그 위치를 넣어만 놓기.
-				else if(map[nr][nc] == 1) {
-					
-				}
-			}
-		}
-		int dist = map[M][N] == 0 ? M*N : map[M][N];
-		
-		// 벽을 부술 수 있는 모든 경우에 대해.
-//		for(int i = 0; i <= M+1; i++) {
-//			for(int j = 0; j <= N+1; j++) {
-//				System.out.print(map[i][j] + " ");
-//			}
-//			System.out.println();
-//		}
-		
-		System.out.println(map[M][N]-1);
+		System.out.println(dist == M*N ? -1 : dist-1);
 	}
 	public static void dfs(int r, int c, int len, int cb) {
 		// 백트래킹 코드
 		if(len > dist) return;
 		
 		// 도착했을때 리턴.
-		else if(r == M && c == N) {
-			dist = Math.min(dist,  len+1);
+		else if(r == M-1 && c == N-1) {
+			dist = Math.min(dist,  len);
 			return;
 		}
 		
@@ -104,10 +65,20 @@ public class BJ2206_벽부수고이동하기 {
 		for(int d = 0; d < 4; d++) {
 			int nr = r + dr[d];
 			int nc = c + dc[d];
-			if(map[nr][nc] <= cb) {				
-				int tmp = map[r][c];
+			if(nr >= 0 && nr < M && nc >= 0 && nc < N && map[nr][nc] <= cb) {
+				int tmp = map[nr][nc];
 				map[nr][nc] = len;
-				dfs(nr, nc, len+1, cb);
+
+//				System.out.println("\n" + nr + " " + nc);
+//				for(int i = 0; i < M; i++) {
+//					for(int j = 0; j < N; j++) {
+//						System.out.print(map[i][j] + " ");
+//					}
+//					System.out.println();
+//				}
+				
+				dfs(nr, nc, len+1, cb-tmp);
+				
 				// 되돌려놓기
 				map[nr][nc] = tmp;
 			}
@@ -123,6 +94,18 @@ public class BJ2206_벽부수고이동하기 {
 0000
 0111
 0000
+
+10 10
+0000010000
+1111010101
+1111000101
+1111111101
+1111111001
+1111111011
+1111000011
+0000000011
+0111111111
+0000000000
 
 4 4
 0111
