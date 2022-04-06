f02b6a85285b7b532c8aa02e28c9a2b0230262de
Your Name <hgchang1@naver.com>
2022-04-06 20:02:06 +0900

    0406

--- eclipse/src/ChangHangeol/BJ2206_벽부수고이동하기.java
+++ eclipse/src/ChangHangeol/BJ2206_벽부수고이동하기.java
@@ -3,8 +3,6 @@ package ChangHangeol;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 // 시간초과
