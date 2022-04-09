76b38a315edaf710557b2554f282a7389ee7589a
Hangeol-Chang <57909592+Hangeol-Chang@users.noreply.github.com>
2022-04-09 15:15:49 +0900

    0409

---
+++ eclipse/src/ChangHangeol/BJ2263_트리순회.java
@@ -0,0 +1,14 @@
+package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ2263_트리순회 {
+    // 중위, 후위순회를 주고, 전위순회를 구하라.
+    public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+    
+    // 분할정복으로 나눠가면서, 하나만 남았을 때 출력하기.
+}
