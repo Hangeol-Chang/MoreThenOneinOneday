3da3d991f8a3724c590cf59af51d7f498c87aad8
Your Name <hgchang1@naver.com>
2022-04-10 15:41:59 +0900

    0410

---
+++ eclipse/src/ChangHangeol/BJ11444_피보나치수6.java
@@ -0,0 +1,25 @@
+package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+public class BJ11444_피보나치수6 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		long[] fibo = new long[1000];
+//		
+//		fibo[1] = 1;
+//		fibo[2] = 1;
+//		for(int i = 3; i < 1000; i++) {
+//			fibo[i] = (fibo[i-1] + fibo[i-2])%1000000007;
+//			System.out.println(i + " " + fibo[i]);
+//		}
+		
+		
+		BigInteger n = new BigInteger(br.readLine());
+		BigDecimal f = new BigDecimal("5");
+		BigInteger ans = (() - (1- f.)) / f.pow(0.5);
+	}
+}
