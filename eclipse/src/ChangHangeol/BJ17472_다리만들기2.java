0cad97f2e384d619bad6571c01c22655a3378c2b
Your Name <hgchang1@naver.com>
2022-04-12 14:35:14 +0900

    0412

--- eclipse/src/ChangHangeol/BJ17472_다리만들기2.java
+++ eclipse/src/ChangHangeol/BJ17472_다리만들기2.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 /*
@@ -48,14 +49,6 @@ public class BJ17472_다리만들기2 {
 		}
 		// 입력 끝.
 		
-		// 출발위치 하나 잡아오기
-		PriorityQueue<loc> pq = new PriorityQueue<>();
-		outer : for(int r = 1; r <= N; r++)
-			for(int c = 1; c <= M; c++)
-				if(map[r][c] == 1) {
-					pq.add(new loc(r, c, 0, 0));
-					break outer;
-				}
 		// 섬의 갯수 세기
 		int islandc = 0;
 		for(int r = 1; r <= N; r++) {
@@ -85,11 +78,28 @@ public class BJ17472_다리만들기2 {
 		}
 		System.out.println("섬 수 : " + islandc);
 		
-		// 계산 시작.
-		while(true) {
-			loc now = pq.poll();
+		// 출발위치 하나 잡아오기
+		Stack<loc> stk = new Stack<>();
+		outer : for(int r = 1; r <= N; r++)
+		for(int c = 1; c <= M; c++)
+			if(map[r][c] == 1) {
+				stk.add(new loc(r, c, 0, 0));
+				break outer;
+			}
+		
+		visited = new boolean[N+2][M+2];
+		int maxlen = Integer.MAX_VALUE;
+		// 계산 시작. dfs
+		while(!stk.isEmpty()) {
+			loc now = stk.pop();
+			// 백트래킹
+			if(now.len > maxlen) continue;
+			// 다리를 다 만들었을 때. 비교해보고 컨티뉴.
+			else if( now.count == islandc-1) { maxlen = Math.min(maxlen, now.len); continue; }
+				
+			System.out.println("현위치 : " + now.r + " " + now.c + ", 현재 길이 : " + now.len);
 			if(visited[now.r][now.c]) continue;
-			visited[now.r][now.c] = true; 
+			visited[now.r][now.c] = true;
 			
 			for(int d = 0; d < 4; d++) {
 				int nr = now.r + dr[d];
@@ -98,20 +108,99 @@ public class BJ17472_다리만들기2 {
 				if(map[nr][nc] == 2) continue;
 				
 				// 주변이 0일 떄와 1일 때로 나눠서, 
-				// 0일때는 가다가 1 만나는지 확인
-				// 1일때는 큐에 넣기
 				switch(map[nr][nc]) {
+				// 0일때는 가다가 1 만나는지 확인
 				case 0 :
 					int len = 0;
 					while(map[nr][nc] == 0) {
 						nr += dr[d];
-						nc += dr[d];
+						nc += dc[d];
 						len++;
 					}
-					if(len > 0 && map[nr][nc] == 1)
-						pq.add(new loc(nr, nc, len + now.len, now.count+1));
+					if(len > 2 && map[nr][nc] == 1 && !visited[nr][nc]) {
+						stk.add(new loc(nr, nc, len + now.len, now.count+1));
+						System.out.println("큐에 넣음0 : " + nr + " " + nc + ", 길이 : " + (len + now.len));						
+					}
+					break;
+				// 1일때는 방문 안했으면 큐에 넣기
+				case 1 :
+					if(!visited[nr][nc]) {
+						System.out.println("큐에 넣음1 : " + nr + " " + nc + ", 길이 : " + now.len);
+						stk.add(new loc(nr, nc, now.len, now.count));
+					}
+					break;
 				}
+				
+				// 원상복구
+				visited[now.r][now.c] = false;
 			}
+			System.out.println();
+			
 		}
+		System.out.println(maxlen == Integer.MAX_VALUE ? -1 : maxlen);
+		// 계산 시작. bfs 폐기
+//		PriorityQueue<loc> pq = new PriorityQueue<>();
+//		outer : for(int r = 1; r <= N; r++)
+//			for(int c = 1; c <= M; c++)
+//				if(map[r][c] == 1) {
+//					pq.add(new loc(r, c, 0, 0));
+//					break outer;
+//				}
+//		while(true) {
+//			loc now = pq.poll();
+//			if(now == null) { System.out.println(-1); return; }
+//			else if(now.count >= islandc-1) { System.out.println(now.len); return; }
+//			
+//			System.out.println("현위치 : " + now.r + " " + now.c + ", 현재 길이 : " + now.len);
+//			if(visited[now.r][now.c]) continue;
+//			visited[now.r][now.c] = true;
+//			
+//			for(int d = 0; d < 4; d++) {
+//				int nr = now.r + dr[d];
+//				int nc = now.c + dc[d];
+//				// 범위 컷.
+//				if(map[nr][nc] == 2) continue;
+//				
+//				// 주변이 0일 떄와 1일 때로 나눠서, 
+//				// 0일때는 가다가 1 만나는지 확인
+//				// 1일때는 큐에 넣기
+//				switch(map[nr][nc]) {
+//				case 0 :
+//					int len = 0;
+//					while(map[nr][nc] == 0) {
+//						nr += dr[d];
+//						nc += dc[d];
+//						len++;
+//					}
+//					if(len > 2 && map[nr][nc] == 1 && !visited[nr][nc]) {
+//						pq.add(new loc(nr, nc, len + now.len, now.count+1));
+//						System.out.println("큐에 넣음0 : " + nr + " " + nc + ", 길이 : " + (len + now.len));						
+//					}
+//					break;
+//				case 1 :
+//					if(!visited[nr][nc]) {
+//						System.out.println("큐에 넣음1 : " + nr + " " + nc + ", 길이 : " + now.len);
+//						pq.add(new loc(nr, nc, now.len, now.count));
+//					}
+//					break;
+//				}
+//			}
+//			System.out.println();
+//		}
 	}
 }
+
+
+/*
+
+7 8
+0 0 0 0 0 0 1 1
+1 1 0 0 0 0 1 1
+1 1 0 0 0 0 0 0
+1 1 0 0 0 1 1 0
+0 0 0 0 0 1 1 0
+0 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1
+
+
+*/
