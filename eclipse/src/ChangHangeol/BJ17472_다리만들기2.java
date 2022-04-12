1a39fcf7273ec38170678ae2e1d62d4c65a1256d
Your Name <hgchang1@naver.com>
2022-04-12 16:54:53 +0900

    0412

--- eclipse/src/ChangHangeol/BJ17472_다리만들기2.java
+++ eclipse/src/ChangHangeol/BJ17472_다리만들기2.java
@@ -33,14 +33,19 @@ public class BJ17472_다리만들기2 {
 	}
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
+	
+	static int[][] map;
+	static boolean[][] visited;
+	static int islandc;
+	static int maxlen = Integer.MAX_VALUE;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		boolean[][] visited = new boolean[N+2][M+2];
-		int[][] map = new int[N+2][M+2];		
+		visited = new boolean[N+2][M+2];
+		map = new int[N+2][M+2];		
 		for(int i = 0; i <= N+1; i++) Arrays.fill(map[i], 2);
 		for(int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -50,7 +55,6 @@ public class BJ17472_다리만들기2 {
 		// 입력 끝.
 		
 		// 섬의 갯수 세기
-		int islandc = 0;
 		for(int r = 1; r <= N; r++) {
 			for(int c = 1; c <= M; c++) {
 				if(!visited[r][c] && map[r][c] == 1) {
@@ -78,65 +82,16 @@ public class BJ17472_다리만들기2 {
 		}
 		System.out.println("섬 수 : " + islandc);
 		
-		// 출발위치 하나 잡아오기
-		Stack<loc> stk = new Stack<>();
+		visited = new boolean[N+2][M+2];
+		// 출발위치 하나 잡아서 시작.
 		outer : for(int r = 1; r <= N; r++)
 		for(int c = 1; c <= M; c++)
 			if(map[r][c] == 1) {
-				stk.add(new loc(r, c, 0, 0));
+				dfs(new loc(r, c, 0, 0));
 				break outer;
 			}
 		
-		visited = new boolean[N+2][M+2];
-		int maxlen = Integer.MAX_VALUE;
 		// 계산 시작. dfs
-		while(!stk.isEmpty()) {
-			loc now = stk.pop();
-			// 백트래킹
-			if(now.len > maxlen) continue;
-			// 다리를 다 만들었을 때. 비교해보고 컨티뉴.
-			else if( now.count == islandc-1) { maxlen = Math.min(maxlen, now.len); continue; }
-				
-			System.out.println("현위치 : " + now.r + " " + now.c + ", 현재 길이 : " + now.len);
-			if(visited[now.r][now.c]) continue;
-			visited[now.r][now.c] = true;
-			
-			for(int d = 0; d < 4; d++) {
-				int nr = now.r + dr[d];
-				int nc = now.c + dc[d];
-				// 범위 컷.
-				if(map[nr][nc] == 2) continue;
-				
-				// 주변이 0일 떄와 1일 때로 나눠서, 
-				switch(map[nr][nc]) {
-				// 0일때는 가다가 1 만나는지 확인
-				case 0 :
-					int len = 0;
-					while(map[nr][nc] == 0) {
-						nr += dr[d];
-						nc += dc[d];
-						len++;
-					}
-					if(len > 2 && map[nr][nc] == 1 && !visited[nr][nc]) {
-						stk.add(new loc(nr, nc, len + now.len, now.count+1));
-						System.out.println("큐에 넣음0 : " + nr + " " + nc + ", 길이 : " + (len + now.len));						
-					}
-					break;
-				// 1일때는 방문 안했으면 큐에 넣기
-				case 1 :
-					if(!visited[nr][nc]) {
-						System.out.println("큐에 넣음1 : " + nr + " " + nc + ", 길이 : " + now.len);
-						stk.add(new loc(nr, nc, now.len, now.count));
-					}
-					break;
-				}
-				
-				// 원상복구
-				visited[now.r][now.c] = false;
-			}
-			System.out.println();
-			
-		}
 		System.out.println(maxlen == Integer.MAX_VALUE ? -1 : maxlen);
 		// 계산 시작. bfs 폐기
 //		PriorityQueue<loc> pq = new PriorityQueue<>();
@@ -187,6 +142,52 @@ public class BJ17472_다리만들기2 {
 //			}
 //			System.out.println();
 //		}
+	}
+	public static void dfs(loc now) {
+		// 백트래킹
+		if(now.len > maxlen) return;
+		// 다리를 다 만들었을 때. 비교해보고 컨티뉴.
+		else if( now.count == islandc-1) { maxlen = Math.min(maxlen, now.len); return; }
+			
+		if(visited[now.r][now.c]) return;
+		visited[now.r][now.c] = true;
+		System.out.println("현위치 : " + now.r + " " + now.c + ", 현재 길이 : " + now.len);
+		
+		for(int d = 0; d < 4; d++) {
+			int nr = now.r + dr[d];
+			int nc = now.c + dc[d];
+			// 범위 컷.
+			if(map[nr][nc] == 2) continue;
+			
+			// 주변이 0일 떄와 1일 때로 나눠서, 
+			switch(map[nr][nc]) {
+			// 0일때는 가다가 1 만나는지 확인
+			case 0 :
+				int len = 0;
+				while(map[nr][nc] == 0) {
+					nr += dr[d];
+					nc += dc[d];
+					len++;
+				}
+				if(len > 2 && map[nr][nc] == 1 && !visited[nr][nc]) {
+					System.out.println("큐에 넣음0 : " + nr + " " + nc + ", 길이 : " + (len + now.len) + ", 다리 수 : " + (now.count+1));
+					dfs(new loc(nr, nc, len + now.len, now.count+1));
+				}
+				break;
+			// 1일때는 방문 안했으면 큐에 넣기
+			case 1 :
+				if(!visited[nr][nc]) {
+					System.out.println("큐에 넣음1 : " + nr + " " + nc + ", 길이 : " + now.len + ", 다리 수 : " + now.count);
+					dfs(new loc(nr, nc, now.len, now.count));
+				}
+				break;
+			}
+			
+		}
+		// 원상복구
+		visited[now.r][now.c] = false;
+		System.out.println();
+		
 	}
 }
 
---
+++ eclipse/src/ChangHangeol/BJ17472_다리만들기2_2.java
@@ -0,0 +1,177 @@
+package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/*
+ 한 섬에서 시작해서, 이 섬에서 갈 수 있는 모든 위치로 다리를 놓아 보고,
+ 길이가 최소인 다리를 선택. 그 다리가 연결된 땅으로 이동하여 반복.
+ 다리를 못 놓은 상황이 확정되면, -1 출력하고 끝내기.
+ */
+public class BJ17472_다리만들기2_2 {
+	static class loc{
+		int r, c;
+		loc(int r, int c){
+			this.r = r;
+			this.c = c;
+		}
+	}
+	static class line implements Comparable<line>{
+		int u, v, w;
+		line(int u, int v, int w){
+			this.u = u;
+			this.v = v;
+			this.w = w;
+		}
+		@Override
+		public int compareTo(line o) {
+			return this.w - o.w;
+		}
+		
+	}
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] p;
+	static int islandc = 1;
+	static int maxlen = Integer.MAX_VALUE;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N+2][M+2];		
+		for(int i = 0; i <= N+1; i++) Arrays.fill(map[i], 2);
+		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 1; j <= M; j++)
+				map[i][j]= Integer.parseInt(st.nextToken());
+		}
+		// 입력 끝.
+		ArrayList<loc>[] islands = new ArrayList[7];
+		for(int i = 1; i <= 6; i++) islands[i] = new ArrayList<>();
+		
+		// 섬의 갯수 세기
+		visited = new boolean[N+2][M+2];
+		for(int r = 1; r <= N; r++) {
+			for(int c = 1; c <= M; c++) {
+				if(!visited[r][c] && map[r][c] == 1) {
+					Queue<int[]> que = new LinkedList<>();
+					que.add(new int[] {r, c});
+					
+					while(!que.isEmpty()) {
+						int[] now = que.poll();
+						if(visited[now[0]][now[1]]) continue;
+						visited[now[0]][now[1]] = true;
+						islands[islandc].add(new loc(now[0], now[1]));
+						
+						for(int d = 0; d < 4; d++) {
+							int nr = now[0] + dr[d];
+							int nc = now[1] + dc[d];
+							if(map[nr][nc] == 2) continue;
+							
+							if(map[nr][nc] == 1 && !visited[nr][nc]) {
+								que.add(new int[] {nr, nc});
+							}
+						}
+					}
+					islandc++;
+				}
+			}
+		}
+//		// 세팅 끝.
+//		for(int i = 1; i < islandc; i++) {
+//			System.out.println(i);
+//			for(loc nd : islands[i]) {
+//				System.out.println("위치들 : " + nd.r + " " + nd.c);
+//			}
+//			System.out.println();
+//		}
+		
+		// 모든 섬에서 섬으로 가는 길 찾기.
+		PriorityQueue<line> lines = new PriorityQueue<>();
+		
+		for(int i = 1; i < islandc; i++) {
+			for(int j = i+1; j < islandc; j++) {
+			// 섬의 모든 좌표에 대해 테스트.
+				int min = Integer.MAX_VALUE;
+				for(loc l1 : islands[i]) {
+					inner : for(loc l2 : islands[j]) {
+						if(l2.r == l1.r && Math.abs(l2.c - l1.c) > 2 && Math.abs(l2.c - l1.c) < min) {
+							int sst = Math.min(l2.c, l1.c);
+							int eed = Math.max(l2.c, l1.c);
+							for(int p = sst+1; p < eed; p++) {
+								if(map[l2.r][p] == 1) continue inner;
+							}
+							min = Math.min(min, Math.abs(l2.c - l1.c));
+						} else if(l2.c == l1.c && Math.abs(l2.r - l1.r) > 2 && Math.abs(l2.r - l1.r) < min) {
+							int sst = Math.min(l2.r, l1.r);
+							int eed = Math.max(l2.r, l1.r);
+							for(int p = sst+1; p < eed; p++) {
+								if(map[p][l2.c] == 1) continue inner;
+							}
+							min = Math.min(min,  Math.abs(l2.r - l1.r));
+						}
+					}
+				}
+				// 구해진 최솟값이 나오는 위치.
+				// 무향 그래프로, 양쪽 다 입력해 줘야 함.
+				if(min != Integer.MAX_VALUE) {
+					lines.add(new line(i, j, min-1));					
+					lines.add(new line(j, j, min-1));
+				}
+			}
+		}
+		// 간선 다 구함 드디어 ㅅㅂ
+		
+		// 크루스칼
+		p = new int[islandc];
+		for(int i = 1; i < islandc; i++) p[i] = i;
+		int select = 0;
+		int ans = 0;
+		while(select < islandc-2 && !lines.isEmpty()) {
+			line now = lines.poll();
+			if(!same(now.u, now.v)) {
+				ans += now.w;
+				union(now.u, now.v);
+				select++;
+			}
+		}
+		System.out.println(select == islandc-2 ? ans : -1 );
+	}
+	public static int findset(int x) {
+		if (p[x] == x) return x;
+		else 		   return p[x] = findset(p[x]);
+	}
+	public static void union(int x, int y) {
+		p[findset(y)] = findset(x);
+	}
+	public static boolean same(int x, int y) {
+		return (findset(x) == findset(y));
+	}
+}
+
+
+/*
+
+7 8
+0 0 0 0 0 0 1 1
+1 1 0 0 0 0 1 1
+1 1 0 0 0 0 0 0
+1 1 0 0 0 1 1 0
+0 0 0 0 0 1 1 0
+0 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1
+
+
+*/
