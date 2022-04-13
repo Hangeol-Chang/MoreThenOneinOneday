ec886cc0a851c898bcb1fa5dada5a3d6bdbe74d4
Your Name <hgchang1@naver.com>
2022-04-13 16:50:20 +0900

    0413

---
+++ eclipse/src/ChangHangeol/BJ2252_줄세우기.java
@@ -0,0 +1,11 @@
+package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ2252_줄세우기 {
+	public static void main(String[] args0) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
