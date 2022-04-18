ed7ff5dbe4a7a0b50be03ef6555abf56f70fcd0b
Your Name <hgchang1@naver.com>
2022-04-18 23:18:27 +0900

    0418

---
+++ eclipse/src/ChangHangeol/BJ10926.java
@@ -0,0 +1,12 @@
+package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ10926 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine() + "??!");
+	}
+}
--- eclipse/src/ChangHangeol/BJ16637_괄호추가하기.java
+++ eclipse/src/ChangHangeol/BJ16637_괄호추가하기.java
@@ -7,56 +7,43 @@ import java.util.ArrayList;
 
 public class BJ16637_괄호추가하기 {
 	static long ans;
-	static ArrayList<Long> nums = new ArrayList<>();
-	static ArrayList<Character> idfs = new ArrayList<>();
+	static int N;
+	static ArrayList<Integer> sel = new ArrayList<>();
+
+	static boolean[] vi;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
+		vi = new boolean[N/2];
 		
 		// 입력받기.
 		String ini = br.readLine();
-		for(int i = 0; i < N; i++) {
-			if(i%2 == 0) nums.add((long) (ini.charAt(i) - '0'));
-			else		 idfs.add(ini.charAt(i));
-		}
+		
 		
 		ans = 0;
-		calc();
+		select(0);
 		System.out.println(ans);
 	}
-	public static void calc() {
-		// 0번에 대해 0, 1계산, 1번에 대해 1, 2계산, 2번에 대해 2, 3 계산.
-		if(idfs.size() == 0) {
-			ans = Math.max(ans, nums.get(0));
+	public static void select(int idx) {
+		if(idx == N) {
+			calc();
 			return;
 		}
-		for(int i = 0; i < idfs.size(); i++) {
-			long num2 = nums.remove(i+1);
-			long num1 = nums.remove(i);
+		for(int i = idx; i < N/2; i++) {
+			if(vi[i]) continue;
+			// 여기 처리한걸로 해주기.
+			vi[i] = true;
+			if(i < N/2 -1) vi[i+1] = true;
 			
-			char idf = idfs.remove(i);
 			
-			long num = 0;
-			switch(idf) {
-			case '+' :
-				num = num1 + num2;
-				break;
-			case '-' :
-				num = num1 - num2;
-				break;
-			case '*' :
-				num = num1 * num2;
-			}
-			nums.add(i, num);
-			
-			// 재귀 실행.
-			calc();
-			
-			// 배열 원상복구
-			nums.remove(i);
-			nums.add(i, num2);
-			nums.add(i, num1);
-			idfs.add(i, idf);
+			// 원상복구
+			vi[i] = false;
+			if(i < N/2 -1) vi[i+1] = false;
 		}
+	}
+	
+	// 후위연산으로 바꾸면서 진행.
+	public static void calc() {
+		
 	}
 }
