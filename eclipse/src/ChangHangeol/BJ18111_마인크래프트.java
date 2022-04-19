63512374bee785add97ac483f4898b2ccb7f80f4
Your Name <hgchang1@naver.com>
2022-04-19 14:10:39 +0900

    0418

--- eclipse/src/ChangHangeol/BJ18111_마인크래프트.java
+++ eclipse/src/ChangHangeol/BJ18111_마인크래프트.java
@@ -3,7 +3,6 @@ package ChangHangeol;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
@@ -12,21 +11,17 @@ import java.util.StringTokenizer;
  * 아무대나 놓을 수 있음. 1초
  */
 public class BJ18111_마인크래프트 {
-	static int R, C, in, mintime, h;
-	static int[] map;
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		in = Integer.parseInt(st.nextToken());
-		h = 0;
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int in = Integer.parseInt(st.nextToken());
 		
 		int maxi = 0;
 		int mini = 256;
-		map = new int[257];
+		int[] map = new int[257];
 		for(int i = 0; i < R; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < C; j++) {
@@ -36,58 +31,39 @@ public class BJ18111_마인크래프트 {
 				mini = Math.min(mini, idx);
 			}
 		}
-		mintime = maxi*500*500*2;
 		// 입력 완료
 
 		// 높이가 일정해 질 때 까지 반복.
-		// 높이를 높이는거 가능??
+		// 높이 낮추는게 이득?
+		// 드는 비용이 같을 땐 무조건 낮추기.
 		// 	높이를 높이는게 이득인가?
 		//	  높이기.
 		//	낮추기.
-		while()
-		
-		run(mini, maxi, 0);
-		
-		// 작업시간 + " " + 맞춰진 높이 출력.
-		System.out.println(mintime + " " + h);
-	}
-	public static void run(int mini, int maxi, int time) {
-		// 백트래킹
-		if(time > mintime) return;
-		// 높이가 맞춰졌을 때.
-		if(mini == maxi) {
-			if(mintime > time) {
-				mintime = time;
-				h = maxi;
-				return;
+		int time = 0;
+		while(mini != maxi) {
+			// 높이는게 이득.
+			if(map[maxi]*2 > map[mini] && in >= map[mini]) {
+				map[mini+1] += map[mini];
+				time += map[mini];
+				mini++;
+			}
+			// 낮추는게 이득.
+			else {
+				map[maxi-1] += map[maxi];
+				time += map[maxi]*2;
+				maxi--;
 			}
 		}
 		
-		// 작동 코드
-		// 아래꺼를 올릴 수 있으면, 아래꺼를 올려서 해보기.
-		if(in >= map[mini]) {
-			// 아래꺼 올리기.
-			int tmp = map[mini];
-			map[mini] = 0;
-			map[mini+1] += tmp;
-			in -= tmp;
-			run(mini+1, maxi, time + tmp);
-			
-			// 원상복구
-			map[mini] = tmp;
-			map[mini+1] -= tmp;
-			in += tmp;
-		}
-		// 위에꺼를 깍아서 해보기.
-		int tmp = map[maxi];
-		map[maxi] = 0;
-		map[maxi-1] += tmp;
-		in += tmp;
-		run(mini, maxi-1, time + 2*tmp);
-		
-		// 원상복구
-		map[maxi] = tmp;
-		map[maxi-1] -= tmp;
-		in -= tmp;
+		// 작업시간 + " " + 맞춰진 높이 출력.
+		System.out.println(time + " " + maxi);
 	}
 }
+
+/*
+
+2 2 35
+20 10
+190 40
+
+*/
