0722eb989c0703d5d30952293ced9f3a9852701f
Your Name <hgchang1@naver.com>
2022-05-01 13:59:36 +0900

    0501

--- eclipse/src/ChangHangeol/BJ25083_새싹.java
+++ eclipse/src/ChangHangeol/BJ25083_새싹.java
@@ -1,3 +1,4 @@
+package ChangHangeol;
 public class BJ25083_새싹 {
     public static void main(String[] args){
         System.out.println("         ,r'\"7");
---
+++ eclipse/src/ChangHangeol/BJ_2022DGIST현풍전산배알고리즘대회_B.java
@@ -0,0 +1,8 @@
+package ChangHangeol;
+
+// 순열
+public class BJ_2022DGIST현풍전산배알고리즘대회_B {
+	public static void main(String[] args) {
+		
+	}
+}
