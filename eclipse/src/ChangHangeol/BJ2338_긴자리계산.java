93133c3203d5830289cafef5e36e4abf3d5ee836
Your Name <hgchang1@naver.com>
2022-05-10 16:56:08 +0900

    0510

---
+++ eclipse/src/ChangHangeol/BJ2338_긴자리계산.java
@@ -0,0 +1,20 @@
+package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class BJ2338_긴자리계산 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		BigInteger a = new BigInteger(br.readLine());
+		BigInteger b = new BigInteger(br.readLine());
+		
+		System.out.println(a.add(b));
+		System.out.println(a.subtract(b));
+		System.out.println(a.multiply(b));
+	}
+}
+
