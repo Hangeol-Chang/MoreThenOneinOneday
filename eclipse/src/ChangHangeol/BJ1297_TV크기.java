2eb09afcc12ada33ff963fa167862998d46653ba
Your Name <hgchang1@naver.com>
2022-05-17 18:12:24 +0900

    0517

---
+++ eclipse/src/ChangHangeol/BJ1297_TV크기.java
@@ -0,0 +1,22 @@
+package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ1297_TV크기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int D = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		
+		double p = Math.sqrt(H*H + W*W);
+		int newW = (int) (W*D / p);
+		int newH = (int) (H*D / p);
+		System.out.print(newH + " " + newW);
+	}
+}
