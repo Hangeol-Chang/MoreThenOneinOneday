33e35f93557492d89e34f33473d8f1a2f6f64480
Your Name <hgchang1@naver.com>
2022-05-24 00:54:03 +0900

    0523

---
+++ eclipse/src/ChangHangeol/BJ11022_AB8.java
@@ -0,0 +1,22 @@
+package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ11022_AB8 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append("Case #" + t + ": " + a + " + " + b + " = " + (a+b) + "\n");
+		}
+		System.out.println(sb);
+	}
+}
