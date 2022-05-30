8ed4d3cbc28d85b5fa92bacef1d46fc675d61e4c
Your Name <hgchang1@naver.com>
2022-05-30 15:45:34 +0900

    0530

---
+++ eclipse/src/Making/TestcaseMaker.java
@@ -0,0 +1,32 @@
+package Making;
+
+public class TestcaseMaker {
+	public static void main(String[] args) {
+		int T = 50;
+		System.out.println(T);
+		
+		for(int t = 1; t <= T; t++) {
+			int n = randN();
+			int k = randK(n);
+			int b = randB(n);
+			
+			System.out.println(n + " " + k + " " + b);
+			
+			for(int i = 0; i < n; i++)
+				System.out.print(randSize() + " ");
+			System.out.println();
+		}
+	}
+	public static int randN() {
+		return (int)(Math.random()*(30-1) + 1);
+	}
+	public static int randK(int n) {
+		return (int)(Math.random()*(n-1) + 1);
+	}
+	public static int randB(int n) {
+		return (int)(Math.random()*(1000-n) + n);
+	}
+	public static int randSize() {
+		return (int)(Math.random()*100);
+	}
+}
