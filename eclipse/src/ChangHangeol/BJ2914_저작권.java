1029d0fbb5d051b496eb8a7b315ecdd44a2de7ec
Your Name <hgchang1@naver.com>
2022-06-02 11:16:04 +0900

    Create BJ2914_저작권.java

---
+++ eclipse/src/ChangHangeol/BJ2914_저작권.java
@@ -0,0 +1,16 @@
+package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2914_저작권 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int l = Integer.parseInt(st.nextToken());
+		System.out.println(n*(l-1) + 1);
+	}
+}
