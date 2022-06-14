ef35fd06599be25ed615a5d2feabc594e324ef16
ChangHangeol <hgchang1@naver.com>
2022-06-14 21:25:16 +0900

    Create BJ2420_사파리월드.java

---
+++ eclipse/src/ChangHangeol/BJ2420_사파리월드.java
@@ -0,0 +1,17 @@
+package ChangHangeol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2420_사파리월드 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Long a = Long.parseLong(st.nextToken());
+		Long b = Long.parseLong(st.nextToken());
+		
+		System.out.print(Math.abs(a - b));
+	}
+}
