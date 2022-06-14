216d89f5a0563d3cd2a08905d908fe039b535c76
ChangHangeol <hgchang1@naver.com>
2022-06-14 23:43:08 +0900

    Update BJ9935_물자열폭발.java

--- eclipse/src/ChangHangeol/BJ9935_물자열폭발.java
+++ eclipse/src/ChangHangeol/BJ9935_물자열폭발.java
@@ -3,65 +3,73 @@ package ChangHangeol;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Stack;
 
 public class BJ9935_물자열폭발 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String ini = br.readLine();
+		char[] ini = br.readLine().toCharArray();
 		char[] ref = br.readLine().toCharArray();
 		Stack<Character> stk = new Stack<>();
-		Stack<Character> tmpstk = new Stack<>();
 		
-		for(int i = ini.length()-1; i >= 0; i--) stk.add(ini.charAt(i));
-		// 문자열 스택화 완료
-		
-		
+		int ilen = ini.length;
 		int reflen = ref.length;
-		int inilen, laslen = 0;
-		do {
-			inilen = stk.size();
+		for(int i = 0; i < ilen; i++) {
+			stk.add(ini[i]);
 			
-			int loc = 0;
-			while(!stk.isEmpty()) {
-				char idf = stk.pop();
-				tmpstk.add(idf);
+//			System.out.println("사이클 시작 : " + stk.toString());
+			
+			if(ini[i] == ref[reflen-1]) {
+				Stack<Character> tmpstk = new Stack<>();
+				int loc = reflen;
 				
-				if(idf == ref[loc]) {
-					if(loc == reflen-1)
-						for(int d = 0; d < reflen; d++) tmpstk.pop();
-					else
-						loc++;
-				}
-				else {
-					loc = 0;
-					if( idf == ref[loc] ) loc = 1;
+				char a;
+				do {
+					loc--;
+					if(loc >= 0 && stk.size() > 0) {
+						a = stk.pop();
+						tmpstk.add(a);						
+					}
+					else break;
 					
+				} while(loc >= 0 && a == ref[loc]);
+				
+				
+				// 여기 오면 통과
+				if(loc < 0) {
+//					System.out.println("성공");
+					continue;
+				}
+				// 여기 오면 실패
+				else {
+//					System.out.println("실패");
+					while(!tmpstk.isEmpty())
+						stk.add(tmpstk.pop());
 				}
 			}
-			
-//			System.out.println(tmpstk.toString());
-			stk.addAll(tmpstk);
-			laslen = stk.size();
-			
-		} while(inilen != laslen);
+//			System.out.println("사이클 끝 : " + stk.toString());
+		}
+		// 연산 끝.
 		
 		if(stk.isEmpty()) System.out.print("FRULA");
 		else {
 			StringBuilder sb = new StringBuilder();
-			while(!stk.isEmpty())
-				sb.append(stk.pop());
-			System.out.println(sb);
+			while(!stk.isEmpty()) sb.append(stk.pop());
+			System.out.print(sb.reverse());
 		}
 	}
 }
 
 /*
 
-a ababa baba caaa a ababa baba
+aaabbb
+ba
+
+aababababacaaaaababababa
 ababa
 
+abcdef
+abd
 
 */
