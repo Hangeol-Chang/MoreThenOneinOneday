b8adfda82bc5a7117f5bd3025c6692b9de4b7a2a
ChangHangeol <hgchang1@naver.com>
2022-06-17 20:13:10 +0900

    0617

---
+++ eclipse/src/BJ2018_수들의합5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num;
+    cin >> num;
+
+    int l = 1;
+    int r = 1;
+
+    int sum = 1;
+    int ans = 0;
+
+    while(r <= num){
+        if(sum < num) sum += ++r;
+        else if(sum > num) sum -= l++;
+        else {
+            ans++;
+            sum -= l++;
+            sum += ++r;
+        }
+        // printf("sum %d\n", sum);
+    }
+    while(sum >= num){
+        sum -= l++;
+        if(sum == num) {
+            ans++;
+            break;
+        }
+    }
+    cout << ans;
+}
