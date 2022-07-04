d22e7f75a9982ecdcc5ed98410814d984381632f
ChangHangeol <hgchang1@naver.com>
2022-07-04 15:27:11 +0900

    Update BJ12015_가장긴증가하는부분수열2.java

--- eclipse/src/BJ12015_가장긴증가하는부분수열2.java
+++ eclipse/src/BJ12015_가장긴증가하는부분수열2.java
@@ -3,33 +3,56 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.stream.Stream;
-/*
- * duelLinkedlist 형태의 priorityque를 강제 구현하여,
- */
+
 public class BJ12015_가장긴증가하는부분수열2 {
+	static ArrayList<Integer> save = new ArrayList<>();
     public static void main(String[] args) throws IOException{
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] nums = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] res = new int[n];
-        ArrayList<Integer> save = new ArrayList<>();
         save.add(0);
         
+        int max = 0;
         // n까지 올라가는 동안 반복.
         for(int i = 0; i < n; i++) {
         	
         	int savesize = save.size();
-        	int j = 0;
-        	while(j < savesize) {
-        		if(save.get(j) > nums[i]) break;
-        		j++;
-        	}
+        	
+        	// lowerbound로 찾아오기.
+        	int j = binarysearch(0, save.size(), nums[i]);
         	
         	// 대입하기
         	if(j == savesize)	save.add(nums[i]);
         	else				save.set(j, nums[i]);
-        	res[i] = j;
+        	max = Math.max(j,  max);
+        	
+//        	for(int k = 0; k < save.size(); k++) {
+//        		System.out.println(k + " " + save.get(k));
+//        	}
+//        	System.out.println(i + "번째 사이클 종료\n");
         }
-        System.out.println(res[n-1]);
+        System.out.println(max);
+    }
+    
+    public static int binarysearch(int st, int ed, int val) {
+    	
+    	int md;
+    	while(st < ed) {
+    		md = (st + ed)/2;
+    		int chk = save.get(md);
+    		if(chk < val) 	   st = md+1;
+    		else if(chk > val) ed = md;
+    		else 			   return md;
+    	}
+    	
+    	return st;
     }
 }
+
+/*
+
+24
+1 2 4 5 8 10 51 1 5 4 7 2 6 8 4 6 41 411 855 74 6 88 100 20
+
+
+*/
