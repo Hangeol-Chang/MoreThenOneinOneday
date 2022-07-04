79575489867590f8f2a0ea23bbead609dbeee5a3
ChangHangeol <hgchang1@naver.com>
2022-07-04 15:27:16 +0900

    Create BJ14938_서강그라운드.cpp

---
+++ eclipse/src/BJ14938_서강그라운드.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+/*
+n개의 위치에 대해 모두 체크해야 함.
+*/
+
+struct vertex {
+    int v;
+    int w;
+};
+
+bool comparevertex(vertex a, vertex b) {
+    
+}
+
+
+int main() {
+    int n, m, r;
+    cin >> n >> m >> r;
+
+    int map[n];
+    for(int i = 0; i < n; i++) {
+        cin >> map[i];
+    }
+
+}
