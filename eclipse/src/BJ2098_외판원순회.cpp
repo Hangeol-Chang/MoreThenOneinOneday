a6da33cba0ca55c084fdfce6d54eed6a89037d5c
ChangHangeol <hgchang1@naver.com>
2022-07-07 09:52:52 +0900

    0707

--- eclipse/src/BJ2098_외판원순회.cpp
+++ eclipse/src/BJ2098_외판원순회.cpp
@@ -1,82 +1,46 @@
 #include <iostream>
 #include <limits>
 
-// gg
+#define INF 400000000
+// dp로 품.
+// 시간초과
 
 using namespace std;
 
 int n;
-int **map;
-int *dp;
+int map[16][16];
+int dp[16][1<<16];
 int fullvisit;
 
 int circuit2(int nowloc, int visited) {
-    cout << "\nnowloc : " << nowloc << " visited : " << visited << "\n";
-    if(nowloc == 0) {
-        if(visited == fullvisit) return -1;
-        else return -2;
+    if(visited == fullvisit) {
+        if(map[nowloc][0] == 0) return INF;
+        return map[nowloc][0];
     }
-
-    if(dp[visited] != 0) {
-        cout << "return" << visited << " " << dp[visited] << "\n";
-        return dp[visited];
-    }
+    if(dp[nowloc][visited] != 0) return dp[nowloc][visited];
 
     //저장되지 않았으면, dp[visited]를 구해야 함.
-    int mincost = numeric_limits<int>::max();
+    dp[nowloc][visited] = INF;
     for(int i = 0; i < n; i++) {
-
-        if(!(visited & 1<<i)) {
-            if(dp[visited] == 0) {
-                int tmpcost = circuit2(i, visited | 1<<i);
-
-                dp[visited] = map[nowloc][i] + circuit2(i, visited | 1<<i);
-            }
-            mincost = min(mincost, dp[visited]);
-        }
-        cout << mincost << "\n";
-
-        for(int i = 0; i <= fullvisit; i++) {
-            cout << dp[i] << " ";
-        }
-        cout << "\n";
+        if((visited & 1<<i) > 0 || map[nowloc][i] == 0) continue;
+        
+        dp[nowloc][visited] = min(dp[nowloc][visited], map[nowloc][i] + circuit2(i, visited | 1<<i));
     }
-    return mincost;
+    return dp[nowloc][visited];
 }
 
 int main() {
-    cin >> n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    map = new int *[n];
+    cin >> n;
     for(int i = 0 ; i < n; i++) {
         fullvisit = fullvisit | 1<<i;
-        map[i] = new int[n];
 
         for(int j = 0; j < n; j++) cin >> map[i][j];
     }
-    dp = new int[fullvisit+1];
-    fill_n(dp, fullvisit+1, 0);
-    // 입력 완료.
-    
-    // for(int i = 0; i < n; i++) {
-    //     for(int j = 0; j < n; j++) {
-    //         cout << map[i][j] << " ";
-    //     }
-    //     cout << "\n";
-    // }
-
-    // for(int i = 0; i < n; i++)
-    //     if(map[i][0] != 0) {
-    //         int bit = 1<<i;
-    //         dp[fullvisit - bit] = map[i][0];
-    //     }
-    
-    // cout << "\nDebug\n";
-    // for(int i = 0; i < fullvisit; i++) {
-    //     cout << dp[i]<< " ";
-    //     if( i % 20 == 0) cout <<endl;
-    // }
-    cout << circuit2(0, 0);
+    cout << circuit2(0, 1);
 }
 
 /*
