8b777ba0038cb6dc3bab7bb02b7899e42a8ff5de
ChangHangeol <hgchang1@naver.com>
2022-07-07 17:27:43 +0900

    Update BJ9328_열쇠.java

--- eclipse/src/BJ9328_열쇠.java
+++ eclipse/src/BJ9328_열쇠.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -13,8 +12,7 @@ public class BJ9328_열쇠 {
 	static int[] dc = {0, 0, -1, 1};
 	
 	static int R, C;
-	static int[][] inimap;
-	static HashSet<Character> inikey;
+	static int[][] map;
 	static ArrayList<door> doors;
 	
 	static class door {
@@ -47,40 +45,41 @@ public class BJ9328_열쇠 {
 			C = Integer.parseInt(st.nextToken());
 			
 			// 필요 변수 초기화.
-			inimap = new int[R][C];
+			map = new int[R+2][C+2];
 			doors = new ArrayList<>();
-			inikey = new HashSet<>();
 			
-			for(int r = 0; r < R; r++) {
+			for(int r = 1; r <= R; r++) {
 				String ini = br.readLine();
-				for(int c = 0; c < C; c++) {
-					inimap[r][c] = ini.charAt(c);
-					if (inimap[r][c] >= 'A' && inimap[r][c] <= 'Z')
+				for(int c = 1; c <= C; c++) {
+					map[r][c] = ini.charAt(c);
+					if (map[r][c] >= 'A' && map[r][c] <= 'Z')
 						// 소문자로 바꿔서 넣음.
-						doors.add(new door(r, c, inimap[r][c] + 32));
+						doors.add(new door(r, c, map[r][c] + 32));
 				}
 			}
-			// 키 입력받아서, 맵에서 지워버리기.
+
+			// 키 입력받아서, 
+			// 맵에서 문 지워버리기.
 			String inkey = br.readLine();
 			if(inkey != "0")
 				for(char key : inkey.toCharArray())
-					for( door d : doors)
-						if(d.req == key)
-							inimap[d.r][d.c] = '.'; 
+					for(door d : doors) if(d.req == key) map[d.r][d.c] = '.'; 
+			
+			// 지워진 문 디버그
+			//
 			
 			// 상하좌우 네 개의 라인에 대해서 진행.
 			int maxdoc = 0;
-			for(int i = 0; i < C; i++)
-				for(int r = 0; r <= R; r += R-1)
-					// 빈칸일때만 입장 가능.
-					if(inimap[r][i] == '.')      maxdoc += circuit(r, i);
-					else if(inimap[r][i] == '$') maxdoc += 1 + circuit(r, i);
+			for(int i = 1; i <= C; i++)
+				for(int r = 1; r <= R; r += R-1)
+					if( (map[r][i] >= 'a' && map[r][i] <= 'z') || map[r][i] == '.' || map[r][i] == '$')
+						maxdoc += circuit(r, i);
 			
-			for(int i = 1; i < R-1; i++)
+			for(int i = 2; i < R; i++)
 				// c = 0; c = C-1에 대해 진행.
-				for(int c = 0; c <= C; c += C-1)
-					if(inimap[i][c] == '.') 	 maxdoc += circuit(i, c);
-					else if(inimap[i][c] == '$') maxdoc += 1 + circuit(i, c);
+				for(int c = 1; c <= C; c += C-1)
+					if( (map[i][c] >= 'a' && map[i][c] <= 'z') || map[i][c] == '.' || map[i][c] == '$')
+						maxdoc += circuit(i, c);
 			
 			sb.append(maxdoc + "\n");
 		}
@@ -91,19 +90,56 @@ public class BJ9328_열쇠 {
 		Queue<location> que = new LinkedList<>();
 		que.add(new location(sr, sc));
 		
-		// 맵, 키 복사세팅
-		int[][] map = new int[R][];
-		for(int i = 0; i < R; i++) map[i] = inimap[i].clone();
-		
 //		System.out.println("start at : " + sr + " " + sc);
-		
 		int doccount = 0;
-		// 루프 시작.
+		
 		while(!que.isEmpty()) {
 			location loc = que.poll();
 			
-			// 방문처리.
-			map[loc.r][loc.c] = '!';
+			// loc에 대한 처리.
+			int locval = map[loc.r][loc.c];
+			if(locval == '$') doccount++;
+			else if( locval >= 'a' && locval <= 'z'){
+				for(door d : doors) {
+					if(d.req == locval) {
+						map[d.r][d.c] = 0;
+						for(int delta = 0; delta < 4; delta++) {
+							int nr = d.r + dr[delta];
+							int nc = d.c + dc[delta];
+							
+							if(map[nr][nc] == 0) que.add(new location(nr, nc));
+						}
+					}
+				}
+			}
+			map[loc.r][loc.c] = 0; 
+			
+			for(int delta = 0; delta < 4; delta++) {
+				
+			}
+		}
+		
+		// 문서 먹으면 +1
+		if(map[sr][sc] == '$') doccount++;
+		// 열쇠를 만났으면 
+		else if(map[sr][sc] >= 'a' && map[sr][sc] <= 'z') {
+			for(door dor : doors)
+				if(dor.req == map[sr][sc]) {
+					map[dor.r][dor.c] = '.';
+					for(int d2 = 0; d2 < 4; d2++) {
+						int nr2 = dor.r + dr[d2];
+						int nc2 = dor.c + dc[d2];
+
+						if(map[nr2][nc2] == 0) que.add(new location(nr2, nc2));
+					}
+				}
+		}
+		map[sr][sc] = '!';
+		
+		
+		// 루프 시작.
+		while(!que.isEmpty()) {
+			location loc = que.poll();
 			
 			// 열쇠를 찾았을 때, 열쇠 주변에 갔던 적이 있으면, 그 위치를 재방문시킴.
 			for(int d = 0; d < 4; d++) {
@@ -116,34 +152,35 @@ public class BJ9328_열쇠 {
 						(map[nr][nc] >= 'A' && map[nr][nc] <= 'Z') ) continue;
 				
 				// 갈 수 있으면 que에 넣기.
-				if(map[nr][nc] == '.') que.add(new location(nr, nc));
-				else if(map[nr][nc] == '$') {
-					map[nr][nc] = '.';
-					inimap[nr][nc] = '*';
-					que.add(new location(nr, nc));
-					doccount++;
-				}
+				if(map[nr][nc] == '.') {}
+				else if(map[nr][nc] == '$') { doccount++; }
+				
 				// 키를 발견했으면
 				else {
 					for(door dor : doors) {
+//						System.out.println(dor.req + " " + map[nr][nc]);
 						if(dor.req == map[nr][nc])
 							map[dor.r][dor.c] = '.';
-							inimap[dor.r][dor.c] = '.'; 
+//							System.out.println(dor.r + " " + dor.c);
 							for(int d2 = 0; d2 < 4; d2++) {
 								int nr2 = dor.r + dr[d2];
 								int nc2 = dor.c + dc[d2];
 								
-								if(nr2 < 0 || nr2 >= R || nc2 < 0 || nc2 >= C) continue;
-								if(map[nr2][nc2] == '!') que.add(new location(nr2, nc2));
+								if(nr2 < 0 || nr2 >= R || nc2 < 0 || nc2 >= C) {
+									map[dor.r][dor.c] = '!'; 
+									que.add(new location(dor.r, dor.c));
+								}
+								else if(map[nr2][nc2] == '!') que.add(new location(nr2, nc2));
 							}
 					}
-					map[nr][nc] = '.';
 				}
 				
+				que.add(new location(nr, nc));
+				map[nr][nc] = '!';
 			}
 //			for(int i = 0; i < R; i++) {
 //				for(int j = 0; j < C; j++) {
-//					System.out.print((char) map[i][j] + " ");
+//					System.out.print((char) map[i][j]);
 //				}
 //				System.out.println();
 //			}
@@ -164,6 +201,69 @@ public class BJ9328_열쇠 {
 *y*x*a*p**$*$**$*
 *****************
 cz
+
+1
+7 7
+*****$*
+..*...*
+*B*...*
+*$*...*
+***..*b
+......*
+......*
+0
+
+1
+4 5
+*A***
+*$*a.
+**$**
+**A**
+0
+
+1
+4 5
+*A***
+*$*.a
+**$**
+**A**
+0
+
+1
+3 3
+***
+*$*
+***
+0
+
+1
+5 10
+**********
+ABCDEF$**a
+**********
+*********c
+**d*e*f*b*
+0
+
+1
+15 15
+**$*.**********
+****.*******$**
+****B.$****b.**
+$*****c*****.**
+*C$.*****fD..**
+*$*xd******.**$
+$.C********A.**
+**h********.AA.
+***************
+***.i**********
+***.***.K$*****
+*k.$$I.$*******
+******.$..j***$
+*******D*******
+****$**F*******
+za
+
 
  */
 
