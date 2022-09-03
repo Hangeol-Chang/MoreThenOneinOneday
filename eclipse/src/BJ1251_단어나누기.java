cc35bc020f0cb74513c5d224c0ab03699b264e00
ChangHangeol <hgchang1@naver.com>
2022-09-03 12:18:01 +0900

    Create BJ1251_단어나누기.java

---
+++ eclipse/src/BJ1251_단어나누기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class BJ1251_단어나누기 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
