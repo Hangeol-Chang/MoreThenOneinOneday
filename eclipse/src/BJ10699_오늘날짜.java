ce8aafdba7091c14f2a29667f735713a91ef37b9
ChangHangeol <hgchang1@naver.com>
2022-09-06 19:34:05 +0900

    Update BJ10699_오늘날짜.java

--- eclipse/src/BJ10699_오늘날짜.java
+++ eclipse/src/BJ10699_오늘날짜.java
@@ -1,13 +1,9 @@
-
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 public class BJ10699_오늘날짜 {
 	public static void main(String[] args) {
-		Date today = new Date();
-		SimpleDateFormat a = new SimpleDateFormat("YYYY-MM-dd");
-		String b = a.format(today);
-		System.out.println(b);
+		LocalDate today = LocalDate.now(ZoneId.of("Asia/Seoul"));
+		System.out.print(today);
 	}
 }
