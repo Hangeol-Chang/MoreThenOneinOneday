21d884d7b6b244f10b6e0a5f6beb8729717ab23a
ChangHangeol <hgchang1@naver.com>
2022-09-08 20:47:10 +0900

    Update BJ20916_안녕.cpp

--- eclipse/src/BJ20916_안녕.cpp
+++ eclipse/src/BJ20916_안녕.cpp
@@ -3,6 +3,23 @@
 
 using namespace std;
 
+
+// 202021
+// 20202021
+// 2020_2021
+
+int targets[] = {202021, 20202021, 
+                202002021, 
+                202012021, 
+                202022021,
+                202032021,
+                202042021,
+                202052021,
+                202062021,
+                202072021,
+                202082021,
+                202092021};
+
 int main() {
     int T;
     cin >> T;
@@ -15,31 +32,44 @@ int main() {
         for(int i = 0; i < N; i++) cin >> arr[i];
 
         sort(arr, arr + N);
-        int ans = 0;        
+        long ans = 0;        
         for(int i = 0; i < N; i++){
             int l = i;
             int r = N-1;
 
-            int target = 202021 - arr[i];
-            int mid;
-            while(l < r) {
-                mid = (l+r)/2;
-                // cout << "l r mid " << l << " " << r << " " << mid << endl;
-                if(arr[mid] < target) l = mid+1;
-                else                   r = mid;
-            }
-            // cout << i << " r : " << r << ", arr[r] : " << arr[r] << ", target : " << target << endl;
+            // 나올 수 있는 모든 결과수에 대해 이분탐색 진행.
+            for(int fulltarget : targets){
+                int target = fulltarget - arr[i];
 
-            if(arr[r] == target)
-                while(arr[r++] == target){
-                    ans++;
-                    // cout << "pass" << endl;
+                int mid;
+                while(l < r) {
+                    mid = (l+r)/2;
+                    // cout << "l r mid " << l << " " << r << " " << mid << endl;
+                    if(arr[mid] < target) l = mid+1;
+                    else                   r = mid;
                 }
+                // cout << i << " r : " << r << ", arr[r] : " << arr[r] << ", target : " << target << endl;
+                
+                if(arr[r] == target)
+                    while(r < N && arr[r++] == target)
+                        ans++;
+            }
         }
         cout << ans << endl;
     }
 }
 /*
+1
+5
+202020 1 1 1 1
+
+1
+5
+1 202020 202020 202020 202020
+
+1
+5
+0 202021 202021 202021 20202021
 
 3
 4
