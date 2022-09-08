b37fb1da2ffcd39ed25acff82085bb1accd4b711
ChangHangeol <hgchang1@naver.com>
2022-09-08 20:57:23 +0900

    Update BJ제1회블롭컵_blobnum.java

--- eclipse/src/BJ제1회블롭컵_blobnum.java
+++ eclipse/src/BJ제1회블롭컵_blobnum.java
@@ -2,19 +2,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.stream.Stream;
+import java.util.StringTokenizer;
 
 public class BJ제1회블롭컵_blobnum {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
-		int[] nums = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		long max = 0;
+		int[] nums = new int[n];
+		int max = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(nums[i], max);
+		}
+
 		for(int i = 1; i < n-1; i++) {
-			long tmp = nums[i] + Math.min(nums[i-1],  nums[i+1]);
+			int tmp = nums[i] + Math.min(nums[i-1],  nums[i+1]);
 			max = Math.max(max,  tmp);
 		}
-		System.out.println(max);
+		System.out.print(max);
 	}
 }
+
+/*
+
+1
+10
+
+6
+2 7 3 10 2 4
+
+
+ */
