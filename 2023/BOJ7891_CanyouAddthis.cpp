8e4e37cf36722bbcc576a845be7660d8ba8277f2
ChangHangeol <hgchang1@naver.com>
2023-06-07 20:51:49 +0900

    Create BOJ7891_CanyouAddthis.cpp

---
+++ 2023/BOJ7891_CanyouAddthis.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    for(int i = 0; i < T; i++) {
+        long long a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+}
