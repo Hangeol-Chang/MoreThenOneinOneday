56773ff8ca43834d8558d7412b9a058b63fdc3e9
ChangHangeol <hgchang1@naver.com>
2023-07-28 18:22:48 +0900

    aa

---
+++ 2023/BOJ1082_방번호.py
@@ -0,0 +1,33 @@
+
+N = int(input())
+price = list(map(int, input().split()))
+wallet = int(input())
+
+maxRoomNum = ['' for i in range(51)]
+
+# 1코스트부터 진행.
+for c in range(1, wallet + 1) :
+    for (num, cost) in enumerate(price):
+        # 현재 가진 코스트 c
+        # 현재 번호 num, num의 가격 cost
+        
+        for i in range(0, c - cost + 1) :
+            now = str(num) + maxRoomNum[i]
+            # print('    ', i, cost, num, now)
+            if(maxRoomNum[c] == '') :
+                maxRoomNum[c] = now
+            else :
+                maxRoomNum[c] = now if int(now) > int(maxRoomNum[c]) else maxRoomNum[c]
+
+    # print(c, maxRoomNum[c])
+
+print(maxRoomNum[wallet])
+
+
+'''
+
+4
+1 8 11
+10
+010
+'''
