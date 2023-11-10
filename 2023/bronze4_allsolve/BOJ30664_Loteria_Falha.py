4684a61c8f5e3c52f00d15184bbc1054d05fe18c
ChangHangeol <hgchang1@naver.com>
2023-11-10 19:21:45 +0900

    1110

---
+++ 2023/bronze4_allsolve/BOJ30664_Loteria_Falha.py
@@ -0,0 +1,9 @@
+
+while True :
+    a = int(input())
+
+    if a == 0 :
+        break
+    
+    if a % 42 == 0 : print("PREMIADO")
+    else : print("TENTE NOVAMENTE")
---
+++ 2023/bronze4_allsolve/BOJ6887_Squares.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    cout << "The largest square has side length " << int(sqrt(num)) << ".";
+}
