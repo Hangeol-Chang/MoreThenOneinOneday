79968900871f9043ef829abaee508e7a97fcbdb8
ChangHangeol <hgchang1@naver.com>
2023-12-08 20:29:13 +0900

    Create BOJ2488.cpp

---
+++ 2023/bronze4_allsolve/BOJ2488.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string t;
+    cin >> t;
+    if(t[0] == 'N' || t[0] == 'n') cout << "Naver D2";
+    else cout << "Naver Whale";
+}
